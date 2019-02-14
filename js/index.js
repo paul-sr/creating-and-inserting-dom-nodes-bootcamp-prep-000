fa47dba5df47482bc557ff075955ad8472e02870
saipaul <saipaul@hotmail.com>
2019-02-14 22:35:03 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+
+var element = document.createElement('div');
+element.innerHTML = 'Hellos!';
+element.style.backgroundColor = '#f9f9f9';
+document.body.appendChild(element);
